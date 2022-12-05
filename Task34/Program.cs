a2d6925be751a8b6964444f376ee2e4314558c51
Irina <bezdnaya@mail.ru>
2022-12-05 17:10:23 +0200

    Посчитали количество чётных элементов в массиве

---
+++ Task34/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+Console.Write("Задайте количество элементов массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];
+int count = 0;
+for (int index = 0; index < N; index++)
+{
+    array[index] = new Random().Next(100, 1000);
+    if (array[index] % 2 == 0)
+        count++;
+}
+Console.WriteLine(string.Join(" ", array));
+Console.WriteLine($"Количество чётных элементов в массиве: {count}");
+
+
