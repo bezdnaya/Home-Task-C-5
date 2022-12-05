3f1030dcd09127b5b9f65d6ad5823b6ba95465de
Irina <bezdnaya@mail.ru>
2022-12-05 18:42:09 +0200

    Исправили ошибку в коде

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,16 +2,16 @@
 // [3 7 22 2 78] -> 76
 
 double [] array = new double [new Random().Next(4, 20)];
-double min = 0;
-double max = array[0];
 for (int i = 0; i < array.Length; i++)
 {
     array[i] = new Random().Next(0, 100);
-    min = array[i];
-    if (array[i] > max)
-        max = array[i];
-    else if (array[i] < min)
-        min = array[i];
 }
+double min = array[0];
+double max = array[0];
+for (int j = 0; j < array.Length; j++)
+    if (array[j] > max)
+        max = array[j];
+    else if (array[j] < min)
+        min = array[j];
 Console.WriteLine(string.Join(" ", array));
 Console.WriteLine($"Разница между максимальным ({max}) и минимальным ({min}) элементами массива {max - min}");
